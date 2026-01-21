683d551b832b21f95bd116f42e2c483d5cb01b98
Deepakdass1326 <deepakdass1326@gmail.com>
2026-01-22 04:59:21 +0530

    Initial backend structure

---
+++ class-2/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+   res.send('Hello this server is running')
+})
+
+app.get('/about',(req, res) => {
+  res.send('this is about')
+})
+
+app.get('/course',(req, res) => {
+  res.send('this is for courses')
+})
+
+
+app.listen(3000,() => {
+    console.log("Server Started");
+    
+})
