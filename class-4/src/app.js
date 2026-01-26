0a62e0e5a28c9a69fa6d117169b087e5d9e080b6
Deepakdass1326 <deepakdass1326@gmail.com>
2026-01-26 22:21:36 +0530

    class-4 completed

---
+++ class-4/src/app.js
@@ -0,0 +1,42 @@
+
+const express = require ("express")
+
+const app = express()
+
+app.use(express.json())
+
+const notes = []
+
+
+app.get('/notes',(req,res)=> {
+    res.send(notes)
+})
+
+
+app.post('/notes',(req, res) => {
+    console.log(req.body);
+    notes.push(req.body)
+    console.log(notes)
+    res.send("notes created")
+})
+
+app.delete('/notes/:index',(req,res)=> {
+    delete notes[req.params.index]
+    res.send("deleted notes")
+})
+
+app.patch("/notes/:index",(req,res) => {
+    notes[req.params.index].description = req.body.description
+    res.send("updated")
+
+    
+})
+
+
+
+
+
+
+
+
+module.exports = app
